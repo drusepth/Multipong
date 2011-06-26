67664c043f52685b4a75e12d893a7c9ee79f9821
Nathan Hwang <beta.entity.k@gmail.com>
2011-06-26 16:25:47 -0700

    Event handling for the paddle

--- frontend/game.js
+++ frontend/game.js
@@ -1,9 +1,10 @@
 // core game engine declarations
 
 var has_ball = false;
-var ball = {loc: {x: 0, y:0}, vel:{x:0, y:0.001}};
+var ball = {loc: {x:0.5, y:0}, vel:{x:0, y:0.04}};
 var ball_radius = 20; // different sized screens?
-var paddle = {loc: 0};
+var paddle = {loc: {x:0, y:0.1}, vel:{x:0, y:0}}; // ignore y stuff
+var paddle_speed = 0.02;
 var w = 0;
 var h = 0;
 
@@ -24,15 +25,20 @@ function start_game() {
 
     var game = {};
     
-    // transform 
     game.play = function(){
+	// draw the ball
 	if(has_ball) {
 	    // move the ball
 	    ball.loc.x += ball.vel.x;
 	    ball.loc.y += ball.vel.y;
 	    // draw the ball
-	    move_to_loc($(".ball"), ball.loc);
+	    move_to_loc($("#main_ball"), ball.loc);
 	}
+	// draw the paddle, draw it all the time
+	paddle.loc.x += paddle.vel.x;
+	move_to_loc($("#main_paddle"), paddle.loc);
+	
+	// do it over and over again
 	setTimeout(game.play, 1000/fps);
     }
 
@@ -42,5 +48,18 @@ function start_game() {
 $(document).ready(function(){
     w = $(window).width();
     h = $(window).height();
+    // ball_radius = ;
     start_game();
+
+    $(document).keydown(function(event){
+	if(event.which == 39) {
+	    paddle.vel.x = paddle_speed;
+	}
+	if(event.which == 37)
+	    paddle.vel.x = -paddle_speed;
+    });
+
+    $(document).keyup(function(event){
+	paddle.vel.x = 0;
+    });
 });
